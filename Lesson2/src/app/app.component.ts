b87d0c581fed616126e740127ad9779d7810d0ac
Kamil Kaplan <kamil.kaplan@mebitech.com>
2022-07-20 23:12:41 +0300

    20.07.2022 Lesson2

---
+++ Lesson2/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Lesson2';
+  firstName = "FirstName";
+  lastName = "LastName";
+  userName = "UserName";
+  password = "Password";
+}
